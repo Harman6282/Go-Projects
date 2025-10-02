c46b257875166ae44a5c0cddd2174fba42fdbecc
Harmans <harmans6282@gmail.com>
2025-10-03 00:40:07 +0530

    complete

--- todo-cli/main.go
+++ todo-cli/main.go
@@ -1,12 +1,11 @@
 package main
 
-import "fmt"
 
 func main(){
 	todos := Todos{}
-	todos.add("Learn React native")
-	todos.add("Learn devops")
-	fmt.Printf("%+v\n\n", todos)
-	todos.delete(0)
-	fmt.Printf("%+v", todos)
+	storage := NewStorage[Todos]("todos.json")
+	storage.Load(&todos)
+	cmdFlags := NewCmdFlags()
+	cmdFlags.Execute(&todos)
+	storage.Save(todos)
 }
